562b8ebfdd49fe76deba2f7ad835bcfec1905c92
Alexandr Shusharin <ashusharin.dev@gmail.com>
2021-11-21 13:42:58 +0500

    Refactor api call

--- app/src/main/java/com/shusharin/movietest/data/datasource/RemoteDataSource.kt
+++ app/src/main/java/com/shusharin/movietest/data/datasource/RemoteDataSource.kt
@@ -1,9 +1,11 @@
 package com.shusharin.movietest.data.datasource
 
 import androidx.lifecycle.LiveData
+import com.shusharin.movietest.data.api.MovieListResponse
 import com.shusharin.movietest.domain.Movie
+import io.reactivex.Single
 
 interface RemoteDataSource {
 
-    fun getAllMovies(): LiveData<List<Movie>>
+    fun getAllMovies(): Single<MovieListResponse>
 }
--- app/src/main/java/com/shusharin/movietest/data/datasource/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/shusharin/movietest/data/datasource/RemoteDataSourceImpl.kt
@@ -2,12 +2,14 @@ package com.shusharin.movietest.data.datasource
 
 import androidx.lifecycle.LiveData
 import com.shusharin.movietest.data.api.ApiClient
+import com.shusharin.movietest.data.api.MovieListResponse
 import com.shusharin.movietest.domain.Movie
+import io.reactivex.Single
 import javax.inject.Inject
 
 class RemoteDataSourceImpl @Inject constructor(private val apiClient: ApiClient):RemoteDataSource {
 
-    override fun getAllMovies(): LiveData<List<Movie>> {
+    override fun getAllMovies(): Single<MovieListResponse> {
        return apiClient.getMovies()
     }
 }
